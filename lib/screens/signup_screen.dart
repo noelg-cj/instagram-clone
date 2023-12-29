98d9ff4dca1581b2094bd1f8aafd8f884e9c879e
Noel George C J <noelgeorge2004@gmail.com>
2023-12-29 11:57:32 +0530

    Signup screen

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/text_field_input.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    _bioController.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32.0,
+          ),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 2,),
+              SvgPicture.asset(
+                "assets/images/ic_instagram.svg",
+                colorFilter: const ColorFilter.mode(
+                  primaryColor,
+                  BlendMode.srcATop,
+                ),
+                height: 64,
+              ),
+              const SizedBox(height: 64,),
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage("https://plus.unsplash.com/premium_photo-1674777843203-da3ebb9fbca0?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    right: 0,
+                    child: IconButton(onPressed: (){ }, icon: const Icon(Icons.add_a_photo)),
+                  )
+                ],
+              ),
+              const SizedBox(height: 24,),
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: "Email",
+                textInputType: TextInputType.emailAddress
+              ),
+              const SizedBox(height: 24,),
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: "Password",
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+              const SizedBox(height: 24,),
+              TextFieldInput(
+                textEditingController: _usernameController,
+                hintText: "Username",
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(height: 24,),
+              TextFieldInput(
+                textEditingController: _bioController,
+                hintText: "Enter a bio...",
+                textInputType: TextInputType.text
+              ),
+              const SizedBox(height: 24,),
+              Container(
+                child: const Text("Sign up"),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 12.0,
+                ),
+                decoration: const ShapeDecoration(shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4))
+                  ), 
+                  color: blueColor
+                ),
+              ),
+              const SizedBox(height: 12,),
+              Flexible(child: Container(), flex: 2,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text("Don't have an account? "),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                  ),
+                  Container(
+                    child: Text("Sign up.", style: TextStyle(fontWeight: FontWeight.bold),),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                  ),
+                ],
+              )
+            ],
+          )
+        )
+      ),
+    );
+  }
+}

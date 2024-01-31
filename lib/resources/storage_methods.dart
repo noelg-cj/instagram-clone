f65fd720f8812e52f45fa9df654dddd63208585b
Noel George C J <noelgeorge2004@gmail.com>
2024-01-31 23:17:27 +0530

    Comment

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
+
+// Storage methods 
 class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
